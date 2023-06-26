afd44b758a47636c02bd93889f1d151420b44833
Zerui-WANG <zerui_wang@yahoo.fr>
2023-06-26 22:01:01 +0200

    provider swiper

---
+++ src/components/CardsSwiper/CardsSwiper.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { ingredients } from "@/data/data";
+import Image from "next/image";
+import { useState } from "react";
+import { EffectCards } from "swiper";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ValidationButton from "../ValidationButton/ValidationButton";
+import { CardContents } from "@/types/generalTypes";
+
+const CardsSwiper = () => {
+  const [currSlide, setCurrSlide] = useState(0);
+
+  const cardContent: CardContents = ingredients;
+
+  const addToCart = () => {
+    const data = localStorage.getItem("cart");
+    if (data) {
+      !data.split(",").find((id) => Number(id) == cardContent[currSlide].id) &&
+        localStorage.setItem(
+          "cart",
+          data + "," + JSON.stringify(cardContent[currSlide].id) + ":1"
+        );
+    } else {
+      localStorage.setItem(
+        "cart",
+        JSON.stringify(cardContent[currSlide].id) + ":1"
+      );
+    }
+  };
+
+  return (
+    <div>
+      <Swiper
+        className="w-72"
+        modules={[EffectCards]}
+        effect={"cards"}
+        grabCursor={true}
+        slidesPerView={1}
+        onSlideChange={(swiper) => setCurrSlide(swiper.activeIndex)}
+      >
+        {cardContent.map((item, i) => {
+          return (
+            <SwiperSlide key={i} className="bg-gray-100 py-20 rounded-lg">
+              <Image
+                src={`/${item.file}`}
+                width={288}
+                height={288}
+                alt={`/${item.name}`}
+              />
+              <div className="flex justify-between py-2 px-6">
+                <p className="font-bold">{item.name}</p>
+                <p>{item.price}€</p>
+              </div>
+              {item.description && (
+                <p className="px-4 italic">{item.description}</p>
+              )}
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <ValidationButton onClick={addToCart} text="Add" />
+    </div>
+  );
+};
+
+export default CardsSwiper;
---
+++ src/data/data.ts
@@ -0,0 +1,5 @@
+export const ingredients = [
+  { id: 0, name: "Beef 1kg", description: "Mega Texas ranch", file: "beef.jpeg", price: 45 },
+  { id: 1, name: "Tomato 1kg", description: "Alpha Tomato farm", file: "tomato.jpeg", price: 15 },
+  { id: 2, name: "Potato 1kg", description: "Beta farm", file: "potato.jpg", price: 8 },
+];
